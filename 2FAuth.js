21a5257261a9c29372b7c3173f5d22c95baad8fa
David Lynch <davidlynch1919@gmail.com>
2023-10-14 16:24:16 -0400

    fixed all issues surrounding the reset feature and the answer validation

--- 2FAuth.js
+++ 2FAuth.js
@@ -1,83 +1,68 @@
 //Playing sound on click for audio cues
 const audio = new Audio(src="zapsplat_multimedia_button_click_004_68776.mp3");
+
 let buttons = document.querySelectorAll("div.btn-group button");
 let buttonClicked = 0;
+
 const patternNumber = 4;
-const dummyArrayLength = 16;
-let number = 0;
-let generatedPattern = [patternNumber];
-let dummyArray = [dummyArrayLength]
-let buttonClickId = [patternNumber];
-console.log(buttons);
-buttons.forEach(button => {
-    button.addEventListener("click", playSoundDisable)
-    });
-;
 
-function playSoundDisable(event){
-    
-    buttonClicked++;
-    checkSolution(event.currentTarget.id);
-    
-    audio.play();
-    if (buttonClicked < 4){
-        event.currentTarget.disabled = true;
-    }
-}
 
-function checkSolution(clickedButtonId)
+let nums = new Set();
+while (nums.size != patternNumber) 
 {
-    for (let i = 0; i < buttonClickId.length; i++)
-    {
-        buttonClickId[i] = clickedButtonId
-        console.log(clickedButtonId)
-        console.log(buttonClickId)
-    }
-
-    for (let i = 0; i < nums.size; i++)
-    {
-
-    }
-    toggleErrorMessage();
+    nums.add(Math.floor(Math.random() * 16) + 1);
 }
 
-//Reset buttons 
-let resetButton = document.getElementById("resetButton");
-resetButton.addEventListener("click", resetButtons);
-function resetButtons(){
-    toggleErrorMessage();
-    buttonClicked = 0;
+const areSetsEqual = (a, b) => a.size === b.size && [...a].every(value => b.has(value));
+let buttonInputs =new Set();
+
+buttons.forEach(button => {
+    button.addEventListener("click", buttonClick)
+});
+
+console.log(nums);
+console.log(buttonInputs);
+
+function buttonClick(event){
+    buttonClicked++;
+    audio.play();
+    event.currentTarget.disabled = true;
+    buttonInputs.add(Number(event.currentTarget.id));
+
+    console.log(buttonInputs);
+
+    if(buttonClicked === 4){
+        checkSolution(buttonInputs);
+    }
+}
+
+function checkSolution(buttonInputs){
     buttons.forEach(button => {
         if (button.disabled == true){
             button.disabled = false;
         }
     })
-}
-
-//toggles messages
-window.onload = function() {
-    window.setTimeout(fadeout, 4000); //4 second fade out
-}
-
-let text = document.getElementById("toggleText");
-function toggleErrorMessage() {
-    if (text.style.display === "none") {
-      text.style.display = "block";
-    } 
-    else {
-      text.style.display = "none";
+    if(!areSetsEqual(buttonInputs,nums)){
+        alert('Please try again');
+        reset();
+    }
+    else{
+        window.location.replace("./HomePage.html");
     }
 }
 
+//Reset buttons 
+let resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", reset);
 
-
-
-const nums = new Set();
-while (nums.size !== patternNumber) 
-{
-    nums.add(Math.floor(Math.random() * 16) + 1)
+function reset(){
+    buttons.forEach(button => {
+        if (button.disabled == true){
+            button.disabled = false;
+        }
+    })
+    buttonClicked = 0;
+    buttonInputs = new Set()
+    
 }
-
-
-
 //from https://dev.to/shantanu_jana/how-to-play-sound-on-button-click-in-javascript-3m48
