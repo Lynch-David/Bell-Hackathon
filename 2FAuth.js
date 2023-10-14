b04bf10317212c1ce55cb9ee33ff2df869adfe92
TarynBeaupre <2173710@johnabbottcollege.net>
2023-10-14 12:33:17 -0400

    Adding error text fadeout

--- 2FAuth.js
+++ 2FAuth.js
@@ -11,12 +11,16 @@ buttons.forEach(button => {
 function playSoundDisable(event){
     buttonClicked++;
     if (buttonClicked > 4){
-        //CHECK SOLUTION
+        checkSolution();
     }
     audio.play();
     if (buttonClicked <= 4){
         event.currentTarget.disabled = true;
     }
+}
+
+function checkSolution(){
+    toggleErrorMessage();
 }
 
 //Reset buttons 
@@ -29,6 +33,21 @@ function resetButtons(){
             button.disabled = false;
         }
     })
+}
+
+//toggles messages
+window.onload = function() {
+    window.setTimeout(fadeout, 4000); //4 second fade out
+}
+
+let text = document.getElementById("toggleText");
+function toggleErrorMessage() {
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } 
+    else {
+      text.style.display = "none";
+    }
 }
 
 
