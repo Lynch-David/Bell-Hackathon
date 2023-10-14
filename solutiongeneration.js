2a51c034b26fae681721049ff34c884e190d9c73
ryanwiwcharyk <124375048+ryanwiwcharyk@users.noreply.github.com>
2023-10-14 13:11:11 -0400

    Fixed bugs in random unique generation

--- solutiongeneration.js
+++ solutiongeneration.js
@@ -3,26 +3,21 @@ const patternNumber = 4;
 const dummyArrayLength = 16;
 let number = 0;
 let generatedPattern = [patternNumber];
-let buttonGrid = document.getElementById("btn-group")
 let dummyArray = [dummyArrayLength]
 
 
-for (let i = 0; i < dummyArray; i++)
+for (let i = 0; i < dummyArray.length; i++)
 {
     dummyArray[i] = number++;
-    dummyArray.sort(()=> Math.random() - 0.5);
+    
 }
+dummyArray.sort(()=> Math.random() - 0.5);
 
 for (let i = 0; i < generatedPattern.length; i++)
 {
     generatedPattern[i] = dummyArray[i];
 }
 
-
-for (let i = 0; i < generatedPattern.length; i++)
-{
-    console.log(generatedPattern[i]);
-}
 
 
     
