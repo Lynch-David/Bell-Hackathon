424a69dcf29af046bc5441d575f51f6623b824ed
ryanwiwcharyk <124375048+ryanwiwcharyk@users.noreply.github.com>
2023-10-14 13:15:09 -0400

    Moved code to single .js file

--- 2FAuth.js
+++ 2FAuth.js
@@ -51,5 +51,26 @@ function toggleErrorMessage() {
     }
 }
 
+const patternNumber = 4;
+const dummyArrayLength = 16;
+let number = 0;
+let generatedPattern = [patternNumber];
+let dummyArray = [dummyArrayLength]
+
+
+for (let i = 0; i < dummyArray.length; i++)
+{
+    dummyArray[i] = number++;
+    
+}
+dummyArray.sort(()=> Math.random() - 0.5);
+
+for (let i = 0; i < generatedPattern.length; i++)
+{
+    generatedPattern[i] = dummyArray[i];
+}
+
+console.log(generatedPattern);
+
 
 //from https://dev.to/shantanu_jana/how-to-play-sound-on-button-click-in-javascript-3m48
