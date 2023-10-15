b51b233e3b7e815ac6689142749092d55cecd41d
David Lynch <davidlynch1919@gmail.com>
2023-10-15 11:31:39 -0400

    some changes to 2FA

--- 2FAuth.js
+++ 2FAuth.js
@@ -1,5 +1,5 @@
 //Declaring sound
-const audio = new Audio(src="zapsplat_multimedia_button_click_004_68776.mp3");
+const audio = new Audio(src = "zapsplat_multimedia_button_click_004_68776.mp3");
 
 let buttons = document.querySelectorAll("div.btn-group button");//selects all buttons
 //initialize counts
@@ -10,10 +10,12 @@ const patternNumber = 4;
 
 //create a Set which is a collection of 4 unique numbers
 let nums = new Set();
-while (nums.size != patternNumber) 
-{
+while (nums.size != patternNumber) {
     nums.add(Math.floor(Math.random() * 16) + 1);
 }
+
+let answer = document.getElementById("answerPage");
+
 //initialize a constant and new Set to evaluate later on
 const areSetsEqual = (a, b) => a.size === b.size && [...a].every(value => b.has(value));
 let buttonInputs = new Set();
@@ -24,8 +26,8 @@ buttons.forEach(button => {
 
 console.log(nums); //TO DO remove me
 console.log(buttonInputs);
-
-function buttonClick(event){
+s
+function buttonClick(event) {
     buttonClicked++;//increase count of the amount of buttons we clicked
     audio.play(); //audio plays on click
     event.currentTarget.disabled = true; //sets the current box to disabled so that we cant click the same one aain
@@ -33,25 +35,26 @@ function buttonClick(event){
 
     console.log(buttonInputs);
 
-    if(buttonClicked === 4){ //after 4 button clicks check for the solution
-        setTimeout(function(){
+    if (buttonClicked === 4) { //after 4 button clicks check for the solution
+        setTimeout(function () {
             checkSolution(buttonInputs);
         }, 250);
     }
 }
 
-function checkSolution(buttonInputs){
+function checkSolution(buttonInputs) {
     buttons.forEach(button => { //reenable all the boxes
-        if (button.disabled == true){
+        if (button.disabled == true) {
             button.disabled = false;
         }
     })
     buttonInputs = Array.from(buttonInputs);
     nums = Array.from(nums);
-    if(buttonInputs[0] != nums[0] || buttonInputs[1] != nums[1] || buttonInputs[2] != nums[2] || buttonInputs[3] != nums[3]){ //evaluates if the sets are equals to each other
+    localStorage.setItem(nums);
+    if (buttonInputs[0] != nums[0] || buttonInputs[1] != nums[1] || buttonInputs[2] != nums[2] || buttonInputs[3] != nums[3]) { //evaluates if the sets are equals to each other
         reset();
     }
-    else{
+    else {
         window.location.replace("./HomePage.html");//if they are equal than congrats you can go to the homepage
     }
 }
@@ -60,45 +63,43 @@ function checkSolution(buttonInputs){
 let resetButton = document.getElementById("resetButton");
 resetButton.addEventListener("click", resetGrid);
 let showText = document.getElementById("centering")//text for amount of tries we have left
-function resetGrid(){
+function resetGrid() {
     buttons.forEach(button => { //again set all the buttons to enabled
-        if (button.disabled == true){
+        if (button.disabled == true) {
             button.disabled = false;
         }
     })
     //reset the Set and amount of button clicks
-    buttonClicked = 0; 
+    buttonClicked = 0;
     buttonInputs = new Set();
 }
-function reset(){
-    tryCount-= 1; //amount of tries -1 because we just failed
+function reset() {
+    tryCount -= 1; //amount of tries -1 because we just failed
     resetGrid();
     showText.style.visibility = "visible" //shows the text that says how many tries we have left
-    showText.innerText="Solution was incorrect. You have " +tryCount+ " tries remaining."
-    if(tryCount === 0)
-    {
+    showText.innerText = "Solution was incorrect. You have " + tryCount + " tries remaining."
+    if (tryCount === 0) {
         showText.style.visibility = "hidden"
         failedLogin(); //call function to restart the 2FA with new values and solution because they failed their 3 attemps
     }
 }
-function failedLogin(){
+function failedLogin() {
     //Makes a new solution to the 2FA
     alert("Sorry, you used up your 3 tries. Please wait 10 seconds and we'll send you a new solution.")
     buttons.forEach(button => { //set all buttons to disabled
         button.disabled = true;
-        });
-        resetButton.disabled = true;
-    setTimeout(function(){
+    });
+    resetButton.disabled = true;
+    setTimeout(function () {
         resetButton.disabled = false;
-        nums = new Set(); 
-        while (nums.size != patternNumber) 
-        {
+        nums = new Set();
+        while (nums.size != patternNumber) {
             nums.add(Math.floor(Math.random() * 16) + 1);
         }
         tryCount = 3; //resets to default options
         buttons.forEach(button => { //set all buttons to disabled
             button.disabled = false;
-            });
+        });
         console.log(nums);
     }, 10000);
 }
